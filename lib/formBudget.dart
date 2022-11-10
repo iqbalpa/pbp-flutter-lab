541e81a81274acd1814d0a59e5d4f154006820de
iqbalpa <iqbalpahlevi16@gmail.com>
2022-11-10 19:33:06 +0700

    Tugas 8: implement UI for Form Budget

--- lib/formBudget.dart
+++ lib/formBudget.dart
@@ -13,101 +13,138 @@ class _FormBudgetState extends State<FormBudget> {
   final _formKey = GlobalKey<FormState>();
   String _judul = "";
   String _nominal = "";
+  String jenis = "Pemasukan";
+  List<String> listJenis = ["Pemasukan", "Pengeluaran"];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Form Budget"),
-      ),
-      drawer: Drawer(
-        child: Column(
-          children: [
-            ListTile(
-              title: const Text("Tambah Budget"),
-              onTap: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => const FormBudget()),
-                );
-              },
-            ),
-            ListTile(
-              title: const Text("Data Budget"),
-              onTap: () {
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const DataBudget()));
-              },
-            ),
-          ],
+        appBar: AppBar(
+          title: Text("Form Budget"),
         ),
-      ),
-      body: Form(
-        key: _formKey,
-        child: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                        labelText: "Judul",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                        )),
-                    onChanged: (String? value) {
-                      setState(() {
-                        _judul = value!;
-                      });
-                    },
-                    onSaved: (String? value) {
-                      setState(() {
-                        _judul = value!;
-                      });
-                    },
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return "Judul tidak boleh kosong!";
-                      }
-                      return null;
-                    },
+        drawer: Drawer(
+          child: Column(
+            children: [
+              ListTile(
+                title: const Text("Tambah Budget"),
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const FormBudget()),
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text("Data Budget"),
+                onTap: () {
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const DataBudget()));
+                },
+              ),
+            ],
+          ),
+        ),
+        body: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          labelText: "Judul",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          )),
+                      onChanged: (String? value) {
+                        setState(() {
+                          _judul = value!;
+                        });
+                      },
+                      onSaved: (String? value) {
+                        setState(() {
+                          _judul = value!;
+                        });
+                      },
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return "Judul tidak boleh kosong!";
+                        }
+                        return null;
+                      },
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                        labelText: "Nominal",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0),
-                        )),
-                    onChanged: (String? value) {
-                      setState(() {
-                        _judul = value!;
-                      });
-                    },
-                    onSaved: (String? value) {
-                      setState(() {
-                        _judul = value!;
-                      });
-                    },
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return "Nominal tidak boleh kosong!";
-                      }
-                      return null;
-                    },
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          labelText: "Nominal",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          )),
+                      onChanged: (String? value) {
+                        setState(() {
+                          _judul = value!;
+                        });
+                      },
+                      onSaved: (String? value) {
+                        setState(() {
+                          _judul = value!;
+                        });
+                      },
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return "Nominal tidak boleh kosong!";
+                        }
+                        return null;
+                      },
+                    ),
                   ),
-                ),
-              ],
+                  ListTile(
+                    title: const Text("Pilih Jenis"),
+                    trailing: DropdownButton(
+                      value: jenis,
+                      items: listJenis.map((String iniJenis) {
+                        return DropdownMenuItem(
+                          value: iniJenis,
+                          child: Text(iniJenis),
+                        );
+                      }).toList(),
+                      onChanged: (String? newJenis) {
+                        setState(() {
+                          jenis = newJenis!;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
-      ),
-    );
+        floatingActionButton: Container(
+          alignment: Alignment.bottomCenter,
+          margin: const EdgeInsets.only(left: 30.0),
+          child: TextButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.blue),
+              padding: MaterialStateProperty.all(
+                const EdgeInsets.all(20.0),
+              ),
+            ),
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {}
+            },
+            child: const Text(
+              "Simpan",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ));
   }
 }
