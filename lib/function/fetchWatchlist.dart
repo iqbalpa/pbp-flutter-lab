44325b38de7bbcd1feb861241df46a08f473a949
iqbalpa <iqbalpahlevi16@gmail.com>
2022-11-23 06:18:21 +0700

    Tugas 9: first watchlist card

--- lib/function/fetchWatchlist.dart
+++ lib/function/fetchWatchlist.dart
@@ -3,15 +3,15 @@ import 'dart:convert';
 import 'package:counter_7/model/watchlist.dart';
 
 Future<List<Watchlist>> fetchWatchlist() async {
-  print(">>> ini manggil fungsi fetchWatchlist");
+  // print(">>> ini manggil fungsi fetchWatchlist");
   var url = Uri.parse("https://iqbal-tugas-2.herokuapp.com/mywatchlist/json/");
   var response = await http.get(url, headers: {
     "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/json",
   });
-  print(">>> response: ${response.body}");
+  // print(">>> response: ${response.body}");
   var data = jsonDecode(utf8.decode(response.bodyBytes));
-  print(">>> data: $data");
+  // print(">>> data: $data");/
   List<Watchlist> watchlist = [];
   for (var d in data) {
     if (d != null) {
--- lib/pages/mywatchlist.dart
+++ lib/pages/mywatchlist.dart
@@ -28,12 +28,32 @@ class _MyWatchlistState extends State<MyWatchlist> {
             } else {
               return ListView.builder(
                   itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(snapshot.data[index].title),
-                      subtitle: Text(snapshot.data[index].release_date),
-                    );
-                  });
+                  itemBuilder: (_, index) => Container(
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 16, vertical: 12),
+                        padding: const EdgeInsets.all(20.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15.0),
+                          boxShadow: const [
+                            BoxShadow(color: Colors.black, blurRadius: 2.0)
+                          ],
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "${snapshot.data![index].title}",
+                              style: const TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                          ],
+                        ),
+                      ));
             }
           }),
     );
