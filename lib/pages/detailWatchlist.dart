f8532be075e86ddad8b3c26cc034daf08bf3c251
iqbalpa <iqbalpahlevi16@gmail.com>
2022-11-23 07:17:43 +0700

    Tugas 9: add back button in detail watchlist page

--- lib/pages/detailWatchlist.dart
+++ lib/pages/detailWatchlist.dart
@@ -28,62 +28,70 @@ class _DetailWatchlistState extends State<DetailWatchlist> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Detail")),
-      drawer: MyDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(children: [
-          Center(
-            child: Text(
-              widget.title,
-              style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+        appBar: AppBar(title: Text("Detail")),
+        drawer: MyDrawer(),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(children: [
+            Center(
+              child: Text(
+                widget.title,
+                style: const TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 8),
-          Row(children: [
-            const Text(
-              "Release Date: ",
-              style: TextStyle(fontWeight: FontWeight.w700),
-            ),
-            Text(widget.release_date),
+            const SizedBox(height: 12),
+            Row(children: [
+              const Text(
+                "Release Date: ",
+                style: TextStyle(fontWeight: FontWeight.w700),
+              ),
+              Text(widget.release_date),
+            ]),
+            const SizedBox(height: 8),
+            Row(children: [
+              const Text("Rating: ",
+                  style: TextStyle(fontWeight: FontWeight.w700)),
+              Text(widget.rating),
+            ]),
+            const SizedBox(height: 8),
+            Row(children: [
+              const Text("Status: ",
+                  style: TextStyle(fontWeight: FontWeight.w700)),
+              if (widget.watched == true)
+                const Text("Watched")
+              else
+                const Text("Not Watched"),
+            ]),
+            const SizedBox(height: 8),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text("Review: ",
+                        style: TextStyle(fontWeight: FontWeight.w700)),
+                    Text(widget.review),
+                  ]),
+            )
           ]),
-          Row(children: [
-            const Text("Rating: ",
-                style: TextStyle(fontWeight: FontWeight.w700)),
-            Text(widget.rating),
-          ]),
-          Row(children: [
-            const Text("Status: ",
-                style: TextStyle(fontWeight: FontWeight.w700)),
-            if (widget.watched == true)
-              const Text("Watched")
-            else
-              const Text("Not Watched"),
-          ]),
-          Align(
-            alignment: Alignment.topLeft,
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text("Review: ",
-                      style: TextStyle(fontWeight: FontWeight.w700)),
-                  Text(widget.review),
-                ]),
-          )
-        ]),
-      ),
+        ),
 
-      // create full button to back to mywatchlist.dart
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        child: const Text("Back"),
-      ),
-    );
+        // create full button to back to mywatchlist.dart
+        floatingActionButton: Container(
+            margin: const EdgeInsets.only(left: 30),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue,
+                minimumSize: const Size.fromHeight(40), // NEW
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Back'),
+            )));
   }
 }
